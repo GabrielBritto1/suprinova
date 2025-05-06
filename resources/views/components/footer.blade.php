09b4319db82b27a398d991d125a8f08af8e90f0a
Gabriel Britto <gabriel_bianchibritto@hotmail.com>
2025-05-05 22:30:38 -0300

    Modificando e acrescentando novos setores

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,19 @@
 <div style="background-color:#f6f6f7" class="mt-3">
     <footer class="container py-5">
         <div class="row">
-            <div class="mb-4">
+            <div class="mb-4 d-flex flex-column justify-content-center col-12 col-md-6">
                 <h2 class="fw-bold text-dark featured alt">Entre em contato</h2>
                 <p class="text-muted col-4">
                     Deixe seu email e nosso consultor de vendas entrará em contato com você.
                 </p>
-                <div class="col-5 col-md-5 mb-3 d-flex">
-                    <input type="email" name="email" id="email" class="form-control" required>
-                    <a href="#" style="background: linear-gradient(to right, #0A306E, #572BFE);" class="enviar-contato btn btn-sm w-25 text-uppercase mx-2 px-3 py-2 fw-bold text-white">
-                        Enviar <i class="bi bi-arrow-right-circle"></i>
-                    </a>
-                </div>
+                <form action="#">
+                    <div class="col-5 col-md-6 mb-3 d-flex">
+                        <input type="email" name="email" id="email" class="form-control col-5 text-muted" required>
+                        <a href="#" style="background: linear-gradient(to right, #0A306E, #572BFE);" class="enviar-contato btn btn-sm text-uppercase mx-2 px-3 py-2 fw-bold text-white">
+                            Enviar
+                        </a>
+                    </div>
+                </form>
             </div>
             <div class="col-6 col-md">
                 <h5>Setores</h5>
